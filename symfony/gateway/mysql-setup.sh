27b29523c27184940f8fb5668f16751e7d537cea
nicorama <nzozol@gmail.com>
2016-02-13 23:19:09 +0100

    Symfony on Docker

---
+++ symfony/gateway/mysql-setup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mysql -uroot -e "CREATE DATABASE gateway";
---
+++ symfony/gateway/test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Robusta Symfony</title>
+</head>
+<body>
+
+<?
+echo ("hello World");
+?>
+</body>
+</html>
